aab60865b85015dde480d8c2d45d950117905072
Chay <chaitanyakumar.bandi@valpo.edu>
2024-05-03 19:50:30 -0400

    Merge unrelated histories

--- sample/recipes/default.rb
+++ sample/recipes/default.rb
@@ -4,13 +4,29 @@
 #
 # Copyright:: 2024, The Authors, All Rights Reserved.
 
-package 'httpd'
-service 'httpd' do
-action [:enable, :start]
+# Cookbook:: my_web_server
+# Recipe:: default
+
+# Install Nginx package
+package 'nginx' do
+  action :install
 end
+
+# Start and enable Nginx service
+service 'nginx' do
+  action [:start, :enable]
+end
+
+# Create a simple HTML file
 file '/var/www/html/index.html' do
-content 'Chef server has been installed'
+  content '<html><body><h1>Hello, Chef!</h1></body></html>'
+  action :create
 end
+
+
+
+
+
 
 
 
