5614a7a104ffeeba4041994b7b9161ff34cf2ca4
cbandi18 <158519333+cbandi18@users.noreply.github.com>
2024-05-06 17:46:47 -0500

    Update metadata.rb

--- mywebserver/metadata.rb
+++ mywebserver/metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Chaitanya'
 maintainer_email 'chaitanyakumar.bandi@valpo.edu'
 license 'All Rights Reserved'
 description 'Installs/Configures mywebserver'
-version '0.1.0'
+version '0.1.1'
 chef_version '< 16.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
