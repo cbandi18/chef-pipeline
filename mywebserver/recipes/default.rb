dd32b5e740d41fce4621ea1a6b02b3f186dc6776
Chay <chaitanyakumar.bandi@valpo.edu>
2024-05-08 23:36:43 -0400

    update

--- mywebserver/recipes/default.rb
+++ mywebserver/recipes/default.rb
@@ -10,12 +10,10 @@
 package 'nginx' do
   action :install
 end
-
 # Start and enable Nginx service
 service 'nginx' do
   action [:start, :enable]
 end
-
 # Create a simple HTML file
 file '/var/www/html/index.html' do
   content '<html><body><h1>Hello, Chef!</h1></body></html>'
