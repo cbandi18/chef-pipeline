8367514a01c1345d7deb4935a14a01f7872d836b
cbandi18 <158519333+cbandi18@users.noreply.github.com>
2024-05-09 12:56:58 -0500

    Update config.rb

--- config.rb
+++ config.rb
@@ -4,7 +4,7 @@ current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
 node_name                "cbandi"
-client_key               "/home/student/.chef/cbandi.pem"
+client_key               "#{current_dir}/.chef/cbandi.pem"
 chef_server_url          "https://api.chef.io/organizations/valpo18"
 cookbook_path            ["#{current_dir}/../cookbooks"]
 
