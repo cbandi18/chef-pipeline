c1992edeb09dd2525896813318427a77123b6637
Chay <chaitanyakumar.bandi@valpo.edu>
2024-05-09 01:41:11 -0400

    Updated the sample

--- mywebserver/metadata.rb
+++ mywebserver/metadata.rb
@@ -1,9 +1,9 @@
-name 'myNginxServer'
-maintainer 'Chaitanya'
+name 'mywebserver'
+maintainer 'ChaitanyaBandi'
 maintainer_email 'chaitanyakumar.bandi@valpo.edu'
 license 'All Rights Reserved'
 description 'Installs/Configures mywebserver'
-version '0.2.0'
+version '0.2.1'
 chef_version '< 16.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
